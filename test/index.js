a0ef6c067ff9a11cb35901e9f5159f7cfc264da5
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-28 13:08:35 +0200

    refactor: revemo non deterministic test

--- test/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const test = require('ava')
-const beautyError = require('..')
-
-test('pretty an error', t => {
-  t.snapshot(beautyError(new Error("Hello darkness my old' friend")))
-})
